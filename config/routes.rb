38157ddcbcf314d99b2943697113117eceb15c89
Reece <Uni@192-168-1-10.tpgi.com.au>
2018-10-21 21:47:25 +1100

    changed posts to images

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :images
   devise_for :users
-  root 'posts#index'
-  resources :posts
+  root 'images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_014945) do
+ActiveRecord::Schema.define(version: 2018_10_21_054842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -37,5 +46,6 @@ ActiveRecord::Schema.define(version: 2018_10_21_014945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
   add_foreign_key "posts", "users"
 end
